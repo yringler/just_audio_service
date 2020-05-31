06a017d8ba49affbf3c424fab28f45537cf5f440
Yehuda Ringler <yrappdev@gmail.com>
2020-05-30 22:59:19 -0400

    WIP: I think position data manager is integrated into position manager

--- lib/position-manager/position-manager.dart
+++ lib/position-manager/position-manager.dart
@@ -2,12 +2,17 @@ import 'package:audio_service/audio_service.dart';
 import 'package:just_audio_service/position-manager/position.dart';
 import 'package:just_audio_service/position-manager/positioned-audio-task.dart';
 import 'package:just_audio_service/position-manager/position-data-manager.dart';
+
 import 'package:rxdart/rxdart.dart';
 
 /// Simplifies knowlage of current position (no more lag after a seek).
 /// Interacts with a [BackgroundAudioTask] (such as [PositionedAudioTask]) to get last
 /// held position of a media.
 class PositionManager {
+  /// How often the position stream is updated from audio_service.
+  /// Note that when seeking (eg, draggin a position slider), the "position" is updated constantly.
+  static const positionUpdateTime = Duration(milliseconds: 50);
+
   // Ensure that seeks don't happen to frequently.
   final BehaviorSubject<Position> _seekingValues = BehaviorSubject.seeded(null);
   final BehaviorSubject<Position> _positionSubject =
@@ -15,25 +20,52 @@ class PositionManager {
 
   final IPositionDataManager positionDataManager;
 
+  /// When a seek is requested, ignore position updates from audio_service for a little bit,
+  /// to give it time to react to the seek.
+  DateTime _ignoreAudioServiceUntil;
+
   PositionManager({this.positionDataManager}) {
     // Make sure that we always keep up to date on audio_service media position.
-    Rx.combineLatest2<PlaybackState, dynamic, Position>(
-            AudioService.playbackStateStream
-                .where((state) => state?.basicState != BasicPlaybackState.none),
-            Stream.periodic(Duration(milliseconds: 20)),
-            (state, _) => _toDisplayPosition(state))
-        .where((position) => position != null)
-        .listen((position) => _positionSubject.value = position);
+    Rx.combineLatest2<PlaybackState, dynamic, PlaybackState>(
+            AudioService.playbackStateStream.where((state) =>
+                (state?.basicState ?? BasicPlaybackState.none) !=
+                BasicPlaybackState.none),
+            Stream.periodic(positionUpdateTime),
+            (state, _) => state)
+        .where((_) => isAudioServiceEventRelevant())
+        .listen((state) => _positionSubject.value = Position(
+            id: AudioService.currentMediaItem.id,
+            position: Duration(milliseconds: state.currentPosition)));
 
     // Seek, but not to often.
     _seekingValues
-        .sampleTime(Duration(milliseconds: 50))
+        .sampleTime(positionUpdateTime)
         .where((position) => position != null)
         .listen(_realSeek);
 
-    _seekingValues
-        .where((position) => position != null)
-        .listen((event) => _positionSubject.value = event);
+    _seekingValues.where((position) => position != null).listen((event) {
+      _positionSubject.value = event;
+      _ignoreAudioServiceUntil = DateTime.now().add(positionUpdateTime * 2);
+    });
+  }
+
+  // An event from audio_service is only relevant if we aren't in the middle of seeking.
+  bool isAudioServiceEventRelevant() {
+    if (_ignoreAudioServiceUntil == null) {
+      return true;
+    }
+
+    if (_ignoreAudioServiceUntil.isBefore(DateTime.now())) {
+      return false;
+    }
+
+    // After the time to ignore audio_service events has passed, set ignore time to null.
+    // This whole is more verbose then just checking against current time, but avoids constantly checking
+    // the current time. This may or may not be a case of premature optimization.
+
+    _ignoreAudioServiceUntil = null;
+
+    return true;
   }
 
   /// Stream of media position, not only of current media.
@@ -44,8 +76,8 @@ class PositionManager {
   /// Updates the current location in given media, for the given ID. If ID is ommited,
   /// will effect current media.
   void seek(Duration location, {String id}) {
-    _seekingValues.add(
-        Position(id: AudioService.currentMediaItem.id, position: location));
+    _seekingValues.add(Position(
+        id: id ?? AudioService.currentMediaItem.id, position: location));
   }
 
   /// Skips the requested time span for the given id. If ID is ommited, will
@@ -65,34 +97,6 @@ class PositionManager {
   void dispose() {
     _seekingValues.close();
     _positionSubject.close();
-  }
-
-  /// Returns the audio service position, only if we aren't seeking right now.
-  Position _toDisplayPosition(PlaybackState state) {
-    if (state == null) {
-      return null;
-    }
-
-    // Return null if we're currently seeking, and we have a diffirent position
-    // to show based on that.
-    if ((state.basicState == BasicPlaybackState.fastForwarding ||
-            state.basicState == BasicPlaybackState.rewinding) &&
-        _seekingValues.value != null &&
-        _seekingValues.value.id == AudioService.currentMediaItem.id) {
-      return null;
-    }
-
-    int position;
-
-    if (state.basicState == BasicPlaybackState.stopped) {
-      position = 0;
-    } else {
-      position = state.currentPosition;
-    }
-
-    return Position(
-        id: AudioService.currentMediaItem?.id,
-        position: Duration(milliseconds: position));
   }
 
   /// Actually change the position of the media (or save what to start at in a diffirent, not
