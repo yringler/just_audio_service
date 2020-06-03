a1317d0337a8ee1fa9e546ee761167912aa6b1a3
Yehuda Ringler <yrappdev@gmail.com>
2020-06-02 21:15:34 -0400

    delete unused control variables

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -7,32 +7,6 @@ import 'package:audio_service/audio_service.dart';
 import 'package:just_audio_service/background/audio-task.dart';
 import 'package:rxdart/rxdart.dart';
 
-MediaControl playControl = MediaControl(
-  androidIcon: 'drawable/ic_action_play_arrow',
-  label: 'Play',
-  action: MediaAction.play,
-);
-MediaControl pauseControl = MediaControl(
-  androidIcon: 'drawable/ic_action_pause',
-  label: 'Pause',
-  action: MediaAction.pause,
-);
-MediaControl skipToNextControl = MediaControl(
-  androidIcon: 'drawable/ic_action_skip_next',
-  label: 'Next',
-  action: MediaAction.skipToNext,
-);
-MediaControl skipToPreviousControl = MediaControl(
-  androidIcon: 'drawable/ic_action_skip_previous',
-  label: 'Previous',
-  action: MediaAction.skipToPrevious,
-);
-MediaControl stopControl = MediaControl(
-  androidIcon: 'drawable/ic_action_stop',
-  label: 'Stop',
-  action: MediaAction.stop,
-);
-
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
