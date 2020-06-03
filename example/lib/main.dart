ee0b962fbbbb32ddb86fe3b9346a5704bc9d883a
Yehuda Ringler <yrappdev@gmail.com>
2020-06-02 21:12:47 -0400

    Update example (re-copy from @ryanheise)

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,3 @@
-/*
- * This code is pretty much streight from Ryan Heise's audio_service example, with
- * very minor changes to use just_audio_service as the background task.
- */
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -11,138 +7,155 @@ import 'package:audio_service/audio_service.dart';
 import 'package:just_audio_service/background/audio-task.dart';
 import 'package:rxdart/rxdart.dart';
 
+MediaControl playControl = MediaControl(
+  androidIcon: 'drawable/ic_action_play_arrow',
+  label: 'Play',
+  action: MediaAction.play,
+);
+MediaControl pauseControl = MediaControl(
+  androidIcon: 'drawable/ic_action_pause',
+  label: 'Pause',
+  action: MediaAction.pause,
+);
+MediaControl skipToNextControl = MediaControl(
+  androidIcon: 'drawable/ic_action_skip_next',
+  label: 'Next',
+  action: MediaAction.skipToNext,
+);
+MediaControl skipToPreviousControl = MediaControl(
+  androidIcon: 'drawable/ic_action_skip_previous',
+  label: 'Previous',
+  action: MediaAction.skipToPrevious,
+);
+MediaControl stopControl = MediaControl(
+  androidIcon: 'drawable/ic_action_stop',
+  label: 'Stop',
+  action: MediaAction.stop,
+);
+
 void main() => runApp(new MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyAppState createState() => new _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Audio Service Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: AudioServiceWidget(child: MainScreen()),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+class MainScreen extends StatelessWidget {
+  /// Tracks the position while the user drags the seek bar.
   final BehaviorSubject<double> _dragPositionSubject =
       BehaviorSubject.seeded(null);
 
   @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-    connect();
-  }
-
-  @override
-  void dispose() {
-    disconnect();
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    switch (state) {
-      case AppLifecycleState.resumed:
-        connect();
-        break;
-      case AppLifecycleState.paused:
-        disconnect();
-        break;
-      default:
-        break;
-    }
-  }
-
-  void connect() async {
-    await AudioService.connect();
-  }
-
-  void disconnect() {
-    AudioService.disconnect();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      home: WillPopScope(
-        onWillPop: () {
-          disconnect();
-          return Future.value(true);
-        },
-        child: new Scaffold(
-          appBar: new AppBar(
-            title: const Text('Audio Service Demo'),
-          ),
-          body: new Center(
-            child: StreamBuilder<ScreenState>(
-              stream: Rx.combineLatest2<MediaItem,
-                      PlaybackState, ScreenState>(
-                  AudioService.currentMediaItemStream,
-                  AudioService.playbackStateStream,
-                  (mediaItem, playbackState) =>
-                      ScreenState(mediaItem, playbackState)),
-              builder: (context, snapshot) {
-                final screenState = snapshot.data;
-                final mediaItem = screenState?.mediaItem;
-                final state = screenState?.playbackState;
-                final basicState = state?.basicState ?? BasicPlaybackState.none;
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    if (mediaItem?.title != null) Text(mediaItem.title),
-                    if (basicState == BasicPlaybackState.none) ...[
-                      audioPlayerButton(),
-                    ] else
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          if (basicState == BasicPlaybackState.playing)
-                            pauseButton()
-                          else if (basicState == BasicPlaybackState.paused)
-                            playButton()
-                          else if (basicState == BasicPlaybackState.buffering ||
-                              basicState == BasicPlaybackState.skippingToNext ||
-                              basicState ==
-                                  BasicPlaybackState.skippingToPrevious)
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: SizedBox(
-                                width: 64.0,
-                                height: 64.0,
-                                child: CircularProgressIndicator(),
-                              ),
-                            ),
-                          stopButton(),
-                        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Audio Service Demo'),
+      ),
+      body: Center(
+        child: StreamBuilder<ScreenState>(
+          stream: _screenStateStream,
+          builder: (context, snapshot) {
+            final screenState = snapshot.data;
+            final queue = screenState?.queue;
+            final mediaItem = screenState?.mediaItem;
+            final state = screenState?.playbackState;
+            final processingState =
+                state?.processingState ?? AudioProcessingState.none;
+            final playing = state?.playing ?? false;
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (queue != null && queue.isNotEmpty)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.skip_previous),
+                        iconSize: 64.0,
+                        onPressed: mediaItem == queue.first
+                            ? null
+                            : AudioService.skipToPrevious,
                       ),
-                    if (basicState != BasicPlaybackState.none &&
-                        basicState != BasicPlaybackState.stopped) ...[
-                      positionIndicator(mediaItem, state),
-                      Text("State: " +
-                          "$basicState".replaceAll(RegExp(r'^.*\.'), '')),
-                    ]
-                  ],
-                );
-              },
-            ),
-          ),
+                      IconButton(
+                        icon: Icon(Icons.skip_next),
+                        iconSize: 64.0,
+                        onPressed: mediaItem == queue.last
+                            ? null
+                            : AudioService.skipToNext,
+                      ),
+                    ],
+                  ),
+                if (mediaItem?.title != null) Text(mediaItem.title),
+                if (processingState == AudioProcessingState.none) ...[
+                  audioPlayerButton(),
+                ] else
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      if (playing) pauseButton() else playButton(),
+                      stopButton(),
+                    ],
+                  ),
+                if (processingState != AudioProcessingState.none &&
+                    processingState != AudioProcessingState.stopped) ...[
+                  positionIndicator(mediaItem, state),
+                  Text("Processing state: " +
+                      "$processingState".replaceAll(RegExp(r'^.*\.'), '')),
+                  StreamBuilder(
+                    stream: AudioService.customEventStream,
+                    builder: (context, snapshot) {
+                      return Text("custom event: ${snapshot.data}");
+                    },
+                  ),
+                  StreamBuilder<bool>(
+                    stream: AudioService.notificationClickEventStream,
+                    builder: (context, snapshot) {
+                      return Text(
+                        'Notification Click Status: ${snapshot.data}',
+                      );
+                    },
+                  ),
+                ],
+              ],
+            );
+          },
         ),
       ),
     );
   }
 
-  RaisedButton audioPlayerButton() =>
-      startButton('AudioPlayer', _audioPlayerTaskEntrypoint);
+  /// Encapsulate all the different data we're interested in into a single
+  /// stream so we don't have to nest StreamBuilders.
+  Stream<ScreenState> get _screenStateStream =>
+      Rx.combineLatest3<List<MediaItem>, MediaItem, PlaybackState, ScreenState>(
+          AudioService.queueStream,
+          AudioService.currentMediaItemStream,
+          AudioService.playbackStateStream,
+          (queue, mediaItem, playbackState) =>
+              ScreenState(queue, mediaItem, playbackState));
 
-  RaisedButton startButton(String label, Function entrypoint) => RaisedButton(
-        child: Text(label),
-        onPressed: () {
-          AudioService.start(
-            backgroundTaskEntrypoint: entrypoint,
-            resumeOnClick: true,
+  RaisedButton audioPlayerButton() => startButton(
+      'AudioPlayer',
+      () => AudioService.start(
+            backgroundTaskEntrypoint: _audioPlayerTaskEntrypoint,
             androidNotificationChannelName: 'Audio Service Demo',
-            notificationColor: 0xFF2196f3,
+            // Enable this if you want the Android service to exit the foreground state on pause.
+            //androidStopForegroundOnPause: true,
+            androidNotificationColor: 0xFF2196f3,
             androidNotificationIcon: 'mipmap/ic_launcher',
-          ).then((_) => AudioService.playFromMediaId(
-              "https://insidechassidus.org/wp-content/uploads/classes/Life Lessons/Avoda/simcha_MM_2007_64bit.mp3"));
-        },
+          ).then((value) => AudioService.playFromMediaId(
+              "https://insidechassidus.org/wp-content/uploads/classes/Life Lessons/Avoda/simcha_MM_2007_64bit.mp3")));
+
+  RaisedButton startButton(String label, VoidCallback onPressed) =>
+      RaisedButton(
+        child: Text(label),
+        onPressed: onPressed,
       );
 
   IconButton playButton() => IconButton(
@@ -167,12 +180,13 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
     double seekPos;
     return StreamBuilder(
       stream: Rx.combineLatest2<double, double, double>(
-          _dragPositionSubject.stream, 
+          _dragPositionSubject.stream,
           Stream.periodic(Duration(milliseconds: 200)),
           (dragPosition, _) => dragPosition),
       builder: (context, snapshot) {
-        double position = snapshot.data ?? state.currentPosition.toDouble();
-        double duration = mediaItem?.duration?.toDouble();
+        double position =
+            snapshot.data ?? state.currentPosition.inMilliseconds.toDouble();
+        double duration = mediaItem?.duration?.inMilliseconds?.toDouble();
         return Column(
           children: [
             if (duration != null)
@@ -195,7 +209,7 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
                   _dragPositionSubject.add(null);
                 },
               ),
-            Text("${(state.currentPosition / 1000).toStringAsFixed(3)}"),
+            Text("${state.currentPosition}"),
           ],
         );
       },
@@ -204,12 +218,14 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
 }
 
 class ScreenState {
+  final List<MediaItem> queue;
   final MediaItem mediaItem;
   final PlaybackState playbackState;
 
-  ScreenState(this.mediaItem, this.playbackState);
+  ScreenState(this.queue, this.mediaItem, this.playbackState);
 }
 
+// NOTE: Your entrypoint MUST be a top-level function.
 void _audioPlayerTaskEntrypoint() async {
   AudioServiceBackground.run(() => AudioTask());
 }
